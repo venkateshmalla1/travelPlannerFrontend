65baacf8eace8f9793283e8a897350025bf3e461
venkateshmalla1 <venky.ece41@gmail.com>
2026-06-23 02:14:57 +0530

    images list is added

--- src/components/TravelCard/TravelCard.js
+++ src/components/TravelCard/TravelCard.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const images = [
+  "https://res.cloudinary.com/dk6x9gpyl/image/upload/v1782160735/Travel_Agency_Social_Media_Post_Design_i0s0of.jpg",
+  "https://res.cloudinary.com/dk6x9gpyl/image/upload/v1782160735/AI_Tool_for_Travellers__Travel_Poster_GPT_Turn_t9zobk.jpg",
+  "https://res.cloudinary.com/dk6x9gpyl/image/upload/v1782160735/download_q1yer9.jpg",
+  "https://res.cloudinary.com/dk6x9gpyl/image/upload/v1782160734/Corporate_Travel_Services_by_Overseas_Travel_mslacb.jpg",
+  "https://res.cloudinary.com/dk6x9gpyl/image/upload/v1782160734/12_Breathtaking_Mountain_Views_Around_the_World_h44qmx.jpg",
+  "https://res.cloudinary.com/dk6x9gpyl/image/upload/v1782160735/download_1_uw1ky5.jpg",
+  "https://res.cloudinary.com/dk6x9gpyl/image/upload/v1782160734/147704062775761455_k6zflh.jpg",
+  "https://res.cloudinary.com/dk6x9gpyl/image/upload/v1782160734/Full_shot_travel_concept_with_landmarks___Premium_AI-generated_image_n1vh3a.jpg"
+];
+
 export default function TravelCard({ trip, isSelected, onClick }) {
   return (
     <div 
@@ -11,7 +22,7 @@ export default function TravelCard({ trip, isSelected, onClick }) {
     >
       <div style={{ height: '140px', overflow: 'hidden' }}>
         <img 
-          src={trip.tripSummary?.destinationImageUrl}
+          src={images[Math.floor(Math.random() * images.length)]}
           alt={trip.tripSummary?.destination} 
           className="w-100 h-100 object-fit-cover" 
         />
